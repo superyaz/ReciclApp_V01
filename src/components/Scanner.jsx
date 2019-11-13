bcc2421e72cd9f306a1eca2560cc5bc0fb312270
superyaz <yacamocursos@hotmail.com>
2019-11-13 09:34:27 -0500

    Se creo la funcionalidad del escanner, este permite realizar el escaneo de codigos de barras y mostrarlos en el DOM, ademas se da estilo en el archivo css para mejorar el posicionamiento visual del scanner

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,18 +1,81 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Quagga from "quagga";
+//import { Link } from "react-router-dom";
 import "../styles/Scanner.css";
 
 class Scanner extends React.Component {
+  componentDidMount() {
+    let _scannerIsRunning = false;
+    function startScanner() {
+      Quagga.init(
+        {
+          inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            constraints: {
+              width: "790",
+              height: "490"
+            },
+            numberOfWorkers: navigator.hardwareConcurrency,
+            target: document.querySelector("#scanner") // Or '#yourElement' (optional)
+          },
+          decoder: {
+            readers: [
+              "code_128_reader",
+              "upc_reader",
+              "upc_e_reader",
+              "ean_reader",
+              "ean_8_reader",
+              "code_39_reader",
+              "code_39_vin_reader",
+              "codabar_reader"
+            ]
+          },
+          locate: true
+        },
+        function(err) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          Quagga.start();
+        }
+      );
+      _scannerIsRunning = true;
+    }
+    Quagga.onDetected(data => {
+      console.log(data);
+      document.querySelector("#codigo").innerHTML = data.codeResult.code;
+    });
+    document.querySelector("#btnStart").addEventListener("click", () => {
+      startScanner();
+    });
+    document.querySelector("#btnStop").addEventListener("click", () => {
+      if (_scannerIsRunning) {
+        Quagga.stop();
+      } else {
+        return (_scannerIsRunning = false);
+      }
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div id="scanner">
-          <h1>Espacio para el scanner</h1>
-          <Link to="/Home">
-            <button type="submit" className="btn btn-success">
-              Regresar
-            </button>
-          </Link>
+        <h1>Escanear Producto</h1>
+        <div id="codigo" className="p-4"></div>
+        <div id="scanner" className="p-4"></div>
+        <div id="btnRegresar">
+          <button id="btnStart" type="submit" className="btn btn-success">
+            Iniciar
+          </button>
+          <button
+            id="btnStop"
+            type="submit"
+            className="btn btn-success ml-2 stop"
+          >
+            Detener
+          </button>
         </div>
       </React.Fragment>
     );
